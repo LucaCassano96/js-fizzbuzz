693688069c9bde5d15d443657b8ab38b1e305d8d
Luca Cassano <lucacassano96@gmail.com>
2023-05-02 17:28:55 +0200

    elementi corretti a schermo

--- javascript/fizbuzz.js
+++ javascript/fizbuzz.js
@@ -5,26 +5,36 @@ console.log(container);
 
 for (let i = 1; i <= 100; i++) {
    
+let text;
+
   if(i % 15 === 0){
+
+        text = "FizzBuzz";
         console.log( "FizzBuzz" );
+        
        }
     
    else if(i % 3 === 0){
+        text = "Fizz";
         console.log( "Fizz" );
+        
    }
 
    else if(i % 5 === 0){
+    text = "Buzz";
     console.log( "Buzz" );
     
-   }
+    }
 
    else{
+    text = i;
     console.log(i);
+
    }
 
     const quadrato = document.createElement ("div");
 
-    quadrato.append(i);
+    quadrato.append(text);
 
     contenitore.append(quadrato);
 
