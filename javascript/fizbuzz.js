0c386b9ef2fef224e2bf347488f1d967a385cb8e
Luca Cassano <lucacassano96@gmail.com>
2023-05-07 17:20:26 +0200

    quadrati colorati

--- javascript/fizbuzz.js
+++ javascript/fizbuzz.js
@@ -5,38 +5,45 @@ console.log(container);
 
 for (let i = 1; i <= 100; i++) {
    
+const quadrato = document.createElement ("div");
+
 let text;
 
   if(i % 15 === 0){
 
         text = "FizzBuzz";
         console.log( "FizzBuzz" );
+        quadrato.classList.add("yellow");
+
         
     }
     
    else if(i % 3 === 0){
         text = "Fizz";
         console.log( "Fizz" );
+        quadrato.classList.add("red"); 
+        
         
    }
 
    else if(i % 5 === 0){
     text = "Buzz";
     console.log( "Buzz" );
+    quadrato.classList.add("green"); 
 
 }
 
    else{
     text = i;
     console.log(i);
+    quadrato.classList.add("blue"); 
 
    }
 
-    const quadrato = document.createElement ("div");
-
-    quadrato.append(text);
+   quadrato.append(text);
 
     contenitore.append(quadrato);
+   
 
 }
 
